8987cbce94f6f30cc6aa6409874eac610d9c05d2
Asus <Asus@DESKTOP-A9J6H6V>
2021-08-02 23:36:32 +0530

    [Ritesh]: sending file to commit

---
+++ src/sample2.java
@@ -0,0 +1,12 @@
+
+public class sample2 {
+	public static void main(String[] args) {
+		
+		System.out.println("Good Morning");
+		System.out.println("Good Morning");
+		System.out.println("Good Morning");
+		System.out.println("Good Morning");
+		System.out.println("Good Morning");
+	}
+
+}
